057a8b6402c5e028e62025bf3c950b3b7bc8b36c
wine <157895987+00nx@users.noreply.github.com>
2026-01-06 17:02:32 +0530

    Update encryptor.cpp

--- src/encryptor.cpp
+++ src/encryptor.cpp
@@ -2,7 +2,6 @@
 // v0.14.2 (c) Alexander 'xaitax' Hagenah
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
-// Define the implementation flag BEFORE including the header
 #define CHACHA20_IMPLEMENTATION
 #include "..\libs\chacha\chacha20.h"
 
@@ -11,14 +10,14 @@
 #include <vector>
 #include <cstdint>
 
-// A 256-bit (32-byte) key.
+// 256-bit (32-byte) key.
 static const uint8_t aKey[32] = {
     0x1B, 0x27, 0x55, 0x64, 0x73, 0x8B, 0x9F, 0x4D,
     0x58, 0x4A, 0x7D, 0x67, 0x8C, 0x79, 0x77, 0x46,
     0xBE, 0x6B, 0x4E, 0x0C, 0x54, 0x57, 0xCD, 0x95,
     0x18, 0xDE, 0x7E, 0x21, 0x47, 0x66, 0x7C, 0x94};
 
-// A 96-bit (12-byte) nonce.
+//  96-bit (12-byte) nonce.
 static const uint8_t aNonce[12] = {
     0x4A, 0x51, 0x78, 0x62, 0x8D, 0x2D, 0x4A, 0x54,
     0x88, 0xE5, 0x3C, 0x50};
@@ -57,3 +56,4 @@ int main(int argc, char *argv[])
     return 0;
 
 }
+
